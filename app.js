de968b60b8ab075dc18e1912f853363125715259
Martín Riesco <martinriesco16@gmail.com>
2020-10-24 12:09:01 -0300

    newChanges

--- app.js
+++ app.js
@@ -1,23 +1,109 @@
-var express = require('express');
-var exphbs  = require('express-handlebars');
- 
+var express = require("express");
+var exphbs = require("express-handlebars");
+const mercadopago = require("mercadopago");
+
 var app = express();
- 
-const PORT = process.env.PORT || 3000
 
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+const PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.render('home');
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+mercadopago.configure({
+    integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
+    access_token:
+        "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398",
 });
 
-app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+let preference = {
+    external_reference: "martinriesco16@gmail.com",
+    items: [],
+    payer: {
+        name: "Lalo",
+        surname: "Landa",
+        email: "test_user_63274575@testuser.com",
+        phone: {
+            area_code: "11",
+            number: 22223333,
+        },
+        address: {
+            zip_code: "1111",
+            street_name: "False",
+            street_number: 123,
+        },
+    },
+    notification_url: `https://mariesco-mp-ecommerce-nodejs.herokuapp.com/notifications`,
+    back_urls: {
+        success: `https://mariesco-mp-ecommerce-nodejs.herokuapp.com/success`,
+        pending: `https://mariesco-mp-ecommerce-nodejs.herokuapp.com/pending`,
+        failure: `https://mariesco-mp-ecommerce-nodejs.herokuapp.com/failure`,
+    },
+    auto_return: "approved",
+    payment_methods: {
+        installments: 6,
+        excluded_payment_methods: [
+            {
+                id: "amex",
+            },
+        ],
+        excluded_payment_types: [
+            {
+                id: "atm",
+            },
+        ],
+    },
+};
+
+app.get("/", function (req, res) {
+    res.render("home");
 });
 
-app.use(express.static('assets'));
- 
-app.use('/assets', express.static(__dirname + '/assets'));
- 
-app.listen(PORT, () => console.log('corriendo en puerto ', PORT));
+app.get("/detail", function (req, res) {
+    let { query } = req;
+    preference.items.push({
+        id: "1234",
+        title: query.title,
+        description: "Dispositivo móvil de Tienda e-commerce",
+        quantity: parseInt(query.unit),
+        unit_price: parseFloat(query.price),
+        picture_url: `https://mariesco-mp-ecommerce-nodejs.herokuapp.com/${query.img}`,
+    });
+    res.render("detail", req.query);
+});
+
+app.post("/mpago", function (req, res) {
+    mercadopago.preferences
+        .create(preference)
+        .then(function (response) {
+            res.redirect(response.response.init_point);
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.redirect("/");
+        });
+});
+
+app.get("/failure", function (req, res) {
+    res.render("failure");
+});
+
+app.get("/pending", function (req, res) {
+    res.render("pending");
+});
+
+app.get("/success", function (req, res) {
+    console.log("la query trae: ", req.query);
+    res.render("success", req.query);
+});
+
+app.post("notifications", function (req, res) {
+    console.log("la notif webook es:", req.query);
+    console.log("o viene por body: ", req);
+    res.status(200).send("OK");
+});
+
+app.use(express.static("assets"));
+
+app.use("/assets", express.static(__dirname + "/assets"));
+
+app.listen(PORT, () => console.log("corriendo en puerto ", PORT));
